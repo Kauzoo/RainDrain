79752b5c3e45726103f86f56918799d6d15c4301
maelh <maelh@student.ethz.ch>
2023-11-18 22:27:38 +0100

    Organized folders

---
+++ Archive/Polygon2D.gd
@@ -0,0 +1,18 @@
+extends Polygon2D 
+
+# Global Constants #
+var X_INCREMENT = 180
+var Y_INCREMENT = 180
+var X_DIRECTION =   0
+var Y_DIRECTION =   0
+var SPEED       =   3
+####################
+
+# Methods #
+func _init():
+	position = Vector2.ZERO
+	return
+	
+func _process(_delta): ## change to delta if perma movement
+	return
+###########
