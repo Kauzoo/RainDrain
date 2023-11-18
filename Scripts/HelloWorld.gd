158ffd4174f644e8d3759d0d60584794cdc59920
maelh <maelh@student.ethz.ch>
2023-11-18 14:05:57 +0100

    Mael was here

--- Scripts/HelloWorld.gd
+++ Scripts/HelloWorld.gd
@@ -4,6 +4,7 @@ extends Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("HelloWorld")
+	print("Mael was here!")
 	pass # Replace with function body.
 
 
